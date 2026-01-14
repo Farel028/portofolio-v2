29ce92093be2d33228cb7f4ed6b0bd874124b3b1
Farel028 <69224916+Farel028@users.noreply.github.com>
2026-01-14 10:19:54 +0700

    Fix global text color for dark theme

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,23 +7,22 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <>
-      <html lang="id">
-        <body>{children}</body>
-        <head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin="anonymous"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Audiowide&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
-            rel="stylesheet"
-          />
-        </head>
-        <body className="font-sans">{children}</body>
-      </html>
-    </>
+    <html lang="id">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Audiowide&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body className="bg-neutral-950 font-sans text-white antialiased">
+        {children}
+      </body>
+    </html>
   );
 }
